dbc750d139db636c0041b9393702ab23c35a7206
Gia <gqm2162@rit.edu>
2021-04-22 14:38:05 +0000

    setup script and filter_backup

---
+++ setup_so.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dir=`pwd`
+
+echo -n "MISP IP: "
+read ip
+
+echo -n "Server port: "
+read port
+
+cp $dir/filter_backup $dir/filter.py
+sed -i "s/MISP_LOCATION/'$ip'/;s/MISP_PORT/$port/" $dir/filter.py
+
+echo -n "Scheduling cronjob"
+
+crontab -l > cronlist > /dev/null
+echo "* * * * * python3 $dir/filter.py" >> cronlist
+crontab cronlist > /dev/null
+rm cronlist
+printf "\n"
